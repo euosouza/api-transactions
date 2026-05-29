90e6aec5f97865ff8c75978776d89fdfc52511bb
Victor Souza <euosouza@gmail.com>
2026-05-29 18:39:08 -0300

    feat: Utilizando os cookies nas rotas e criando middleware de checar session_id

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,29 +2,39 @@ import type { FastifyInstance } from "fastify";
 import { randomUUID } from "node:crypto";
 import z from "zod";
 import { knex } from "../database";
+import { checkSessionId } from "../middlewares/check-session-id.middleware";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/", async (request, reply) => {
-    const transactions = await knex("transactions").select("*");
+  // Adiciona o middleware checkSessionId a todos os métodos abaixo
+  // app.addHook("preHandler", checkSessionId);
 
-    return reply.status(200).send(transactions);
+  app.get("/", { preHandler: [checkSessionId] }, async (request) => {
+    const sessionId = request.cookies.sessionId;
+
+    const transactions = await knex("transactions").select("*").where("session_id", sessionId).orderBy("created_at", "desc");
+
+    return { transactions };
   });
 
-  app.get("/:id", async (request, reply) => {
+  app.get("/:id", { preHandler: [checkSessionId] }, async (request, reply) => {
+    const sessionId = request.cookies.sessionId;
+
     const transactionsParamsSchema = z.object({
       id: z.string().uuid(),
     });
 
     const { id } = transactionsParamsSchema.parse(request.params);
 
-    const transaction = await knex("transactions").where({ id }).first();
+    const transaction = await knex("transactions").where({ id, session_id: sessionId }).first();
 
     return reply.status(200).send(transaction);
   });
 
-  app.get("/summary", async (request, reply) => {
-    const summary = await knex("transactions").sum("amount", { as: "amount" }).first();
+  app.get("/summary", { preHandler: [checkSessionId] }, async (request, reply) => {
+    const sessionId = request.cookies.sessionId;
+
+    const summary = await knex("transactions").where("session_id", sessionId).sum("amount", { as: "amount" }).first();
 
     return reply.status(200).send({
       amount: parseFloat(Number(summary?.amount ?? 0).toFixed(2)),
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,15 @@ import fastify from "fastify";
 import { env } from "./env/index";
 import { transactionsRoutes } from "./routes/transactions.routes";
 
-const app = fastify();
+const app = fastify({
+  logger: true,
+});
 const PORT = env.PORT;
 
 app.register(cookie);
-app.register(transactionsRoutes, { prefix: "transactions" });
+app.register(transactionsRoutes, {
+  prefix: "transactions",
+});
 
 app.listen({ port: PORT }).then(() => {
   console.log(`🔥 Servidor está rodando na porta ${PORT}`);
